60710ba4083dc4c09703c403358565050e3ebc92
Chad Smith <Chad@Chads-iMac.local>
2014-07-01 20:25:49 -0700

    added a test class

---
+++ HHTest.h
@@ -0,0 +1,13 @@
+//
+//  HHTest.h
+//  Git Test
+//
+//  Created by Chad Smith on 7/1/14.
+//  Copyright (c) 2014 Hidden Hilltop Developers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HHTest : NSObject
+
+@end
---
+++ HHTest.m
@@ -0,0 +1,13 @@
+//
+//  HHTest.m
+//  Git Test
+//
+//  Created by Chad Smith on 7/1/14.
+//  Copyright (c) 2014 Hidden Hilltop Developers. All rights reserved.
+//
+
+#import "HHTest.h"
+
+@implementation HHTest
+
+@end
