e559a659830385cbbcf57701e0ebff7e95459212
Chad Smith <Chad@Chads-iMac.local>
2014-07-01 19:54:08 -0700

    Initial Commit

---
+++ Git Test/HHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HHAppDelegate.h
+//  Git Test
+//
+//  Created by Chad Smith on 7/1/14.
+//  Copyright (c) 2014 Hidden Hilltop Developers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Git Test/HHViewController.h
@@ -0,0 +1,13 @@
+//
+//  HHViewController.h
+//  Git Test
+//
+//  Created by Chad Smith on 7/1/14.
+//  Copyright (c) 2014 Hidden Hilltop Developers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HHViewController : UIViewController
+
+@end
